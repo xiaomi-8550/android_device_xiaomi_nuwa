11bbd668a43fae343846db75f4fcedfe79430fc5
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-04-23 15:27:41 +0200

    nuwa: Add support for ESIM (eUICC)
    Change-Id: I3484dafe338fd3448815657447e5901534eb60c4

--- device.mk
+++ device.mk
@@ -27,3 +27,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml
+
+# Telephony
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
